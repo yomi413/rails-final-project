c08bb5a66ba369b1b27d7dd11a789d108d20192d
Yomaira Escano <yjescano@gmail.com>
2018-06-10 16:35:25 -0400

    Created tables/databases

--- db/migrate/20180610200347_create_cars.rb
+++ db/migrate/20180610200347_create_cars.rb
@@ -1,6 +1,12 @@
 class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.integer :year
+      t.string :color
+      t.string :size
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20180610200534_create_parking_spaces.rb
+++ db/migrate/20180610200534_create_parking_spaces.rb
@@ -1,6 +1,8 @@
 class CreateParkingSpaces < ActiveRecord::Migration[5.2]
   def change
     create_table :parking_spaces do |t|
+      t.integer :space_number
+      t.integer :car_id
 
       t.timestamps
     end
