18c63e77cd9f0a1b30cd4f1289471f490f79db0c
Yomaira Escano <yjescano@gmail.com>
2018-06-11 13:32:33 -0400

    Added sign up and login links to home page; added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :cars
 
   root "welcome#home"
+
+  get '/session', to: 'session#new'
+  post '/session', to: 'session#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
