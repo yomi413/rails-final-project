33f02b3b4afaebb4817c117fb7921244301d2470
Yomaira Escano <yjescano@gmail.com>
2018-06-11 15:46:41 -0400

    Added the new and create actions to the Cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,15 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :color, :size)
+  end
 end
