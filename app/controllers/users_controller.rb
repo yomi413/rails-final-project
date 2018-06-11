08c61bbc773aed2b573c3ba561c40de9b8ecb47a
Yomaira Escano <yjescano@gmail.com>
2018-06-11 19:00:49 -0400

    Updated user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    redirect_to user_path(@user)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :image, :uid)
+    params.require(:user).permit(:name, :email, :image, :uid)
   end
 end
