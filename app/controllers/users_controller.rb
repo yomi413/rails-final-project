e40c1d2712abb07c51abf90e6b5f9a9a92c11ed7
Yomaira Escano <yjescano@gmail.com>
2018-06-11 16:20:54 -0400

    Updated user_params; added redirect path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    redirect_to user_path(@user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :image, :uid)
+    params.require(:user).permit(:name, :email, :password, :image, :uid)
   end
 end
