a4c58f5c60bd107e77da6fb054c05102dba72213
Yomaira Escano <yjescano@gmail.com>
2018-06-11 16:08:54 -0400

    Added the new and create actions to the Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image, :uid)
+  end
 end
