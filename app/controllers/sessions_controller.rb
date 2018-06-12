b2373cecb16cb123836a5ea0b8e3fa776005682f
Yomaira Escano <yjescano@gmail.com>
2018-06-12 12:48:55 -0400

    Slight modification to the create action:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by(email: params[:emal])
 
     if @user && @user.authenticate[params[:password]]
       session[:user_id] = @user.id
