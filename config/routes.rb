5c352ce422e08fe6da0d5e2d55e52c51e0117136
Yomaira Escano <yjescano@gmail.com>
2018-06-11 21:59:13 -0400

    Updated sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root "welcome#home"
 
-  get '/session', to: 'session#new'
-  post '/session', to: 'session#create'
+  get '/session', to: 'sessions#new'
+  post '/session', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
