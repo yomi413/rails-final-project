945f76c1c65dbfcd97ac28810f022f9ff4606b31
Yomaira Escano <yjescano@gmail.com>
2018-06-12 00:14:03 -0400

    Updated create action; added destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(id: params[:id])
+
+    if @user && @user.authenticate[params[:password]]
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
