6649a58fa3b1e65433872b706b7406a854550c5d
Yomaira Escano <yjescano@gmail.com>
2018-06-12 00:11:29 -0400

    Added the route to delete a session

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get '/session', to: 'sessions#new'
   post '/session', to: 'sessions#create'
+  delete '/session', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
