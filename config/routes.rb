cfbe63dd875d56fbc399718f64e1bdcbe21bb7b3
Yomaira Escano <yjescano@gmail.com>
2018-06-12 22:46:23 -0400

    Minor modification to the login route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root "welcome#home"
 
   get '/session', to: 'sessions#new'
-  post '/session', to: 'sessions#create'
+  post '/session', to: 'sessions#create', as: 'login'
   delete '/session', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
