81ab490d43476f1929f5ecc11c0c4bd030a7edd9
Yomaira Escano <yjescano@gmail.com>
2018-06-12 21:08:35 -0400

    Added validations to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_one :parking_space
+
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
 end
