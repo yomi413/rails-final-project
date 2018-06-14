c5e64571834166a51f97403d73ceeec20c170e76
Yomaira Escano <yjescano@gmail.com>
2018-06-13 23:20:37 -0400

    Added Google omniauth login link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_user
 
+  def verified_user
+    redirect_to '/' unless logged_in?
+  end
+
   def logged_in?
     !!current_user
   end
 
   def current_user
-    @current_user || User.find_by(id: session[:user_id]) if session[:user_id]
+    User.find_by(id: session[:user_id])
   end
 end
