0c755ac533f0c6056c5aecde55d36bbec0afd431
Yomaira Escano <yjescano@gmail.com>
2018-06-13 22:49:48 -0400

    Added omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get '/session', to: 'sessions#new'
   post '/session', to: 'sessions#create'
   delete '/session', to: 'sessions#destroy'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
