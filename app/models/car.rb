9bd71db7825ddfd8681b1dde2f872b41d1aaa48a
Yomaira Escano <yjescano@gmail.com>
2018-06-15 16:37:48 -0400

    Added accepts_nested_attributes_for method

--- app/models/car.rb
+++ app/models/car.rb
@@ -8,6 +8,8 @@ class Car < ApplicationRecord
   validates :color, presence: true
   validates :size, presence: true
 
+  accepts_nested_attributes_for :parking_space
+
   def your_car
     self.make + " " + self.model + " " + "(" + self.year.to_s + ")"
   end
