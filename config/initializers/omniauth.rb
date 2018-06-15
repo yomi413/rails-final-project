79c3ecba12bc42ce61e3db4adcc8b9a2c65926af
Yomaira Escano <yjescano@gmail.com>
2018-06-14 21:56:56 -0400

    Modified omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do |variable|
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CONSUMER_KEY'], ENV['GOOGLE_CONSUMER_SECRET']
 end
