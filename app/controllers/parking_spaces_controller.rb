2d5b323fcf65b2fed88beb1bf1b2ca166f4d15a9
Yomaira Escano <yjescano@gmail.com>
2018-06-15 16:00:43 -0400

    Added create action

--- app/controllers/parking_spaces_controller.rb
+++ app/controllers/parking_spaces_controller.rb
@@ -1,2 +1,13 @@
 class ParkingSpacesController < ApplicationController
+  def create
+    @parking_space = ParkingSpace.create(parking_space_params)
+    @user = User.find_by(id: params[:id])
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def parking_space_params
+    params.require(:parking_space).permit(:space_number, :car_id)
+  end
 end
