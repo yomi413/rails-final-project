5438ae38c195a3d8748037dd11d0c21c03dcdd1f
Yomaira Escano <yjescano@gmail.com>
2018-07-03 23:19:27 -0400

    Fixed the password validation for functional omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_many :cars
-  # has_many :parking_spaces, through: :cars
+  has_many :parking_spaces, through: :cars
 
-  has_secure_password
+  has_secure_password(validations: false)
 
   validates :name, presence: true
   validates :email, presence: true
+  # validates :password, presence: true
 
   def first_name
     self.name.split[0]
