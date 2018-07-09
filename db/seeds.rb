fc7296b02736361f3cdc40684d0cd838e199040d
Yomaira Escano <yjescano@gmail.com>
2018-07-09 12:17:35 -0400

    Added new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+alicia = User.create(:name => "Alicia Maribel", :email => "alicia@abc.com", :password => "vacation")
+daniel = User.create(:name => "Daniel Day", :email => "daniel@abc.com", :password => "birthday")
+
+parking_spaces = (100..125).to_a
+
+alicia.cars.create(:make => "Honda", :model => "Odyssey", :year => "2010", :color => "black", :size => "minivan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+alicia.cars.create(:make => "Toyota", :model => "Camry", :year => "2004", :color => "blue", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Volkswagen", :model => "Golf", :year => "2014", :color => "green", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Mercedes", :model => "Benz", :year => "2015", :color => "white", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+alicia.cars.create(:make => "Nissan", :model => "Sentra", :year => "2012", :color => "gold", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Hyundai", :model => "Santa Fe", :year => "2080", :color => "green", :size => "SUV", :parking_space_attributes => {:space_number => parking_spaces.pop})
+
+alicia.cars.create(:make => "Honda", :model => "Odyssey", :year => "2010", :color => "black", :size => "minivan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+alicia.cars.create(:make => "Toyota", :model => "Camry", :year => "2004", :color => "blue", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Volkswagen", :model => "Golf", :year => "2014", :color => "green", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Mercedes", :model => "Benz", :year => "2015", :color => "white", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+alicia.cars.create(:make => "Nissan", :model => "Sentra", :year => "2012", :color => "gold", :size => "sedan", :parking_space_attributes => {:space_number => parking_spaces.pop})
+daniel.cars.create(:make => "Hyundai", :model => "Santa Fe", :year => "2080", :color => "green", :size => "SUV", :parking_space_attributes => {:space_number => parking_spaces.pop})
+
