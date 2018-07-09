501e15fba38369c8943836103d87b2c57252bcb8
Yomaira Escano <yjescano@gmail.com>
2018-07-09 16:40:02 -0400

    Added admin to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :uid)
+    params.require(:user).permit(:name, :email, :password, :uid, :admin)
   end
 end
