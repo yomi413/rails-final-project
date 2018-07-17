6271564b1e1fad7a7887153a3d3f80d2c8480220
Yomaira Escano <yjescano@gmail.com>
2018-07-17 01:17:48 -0400

    Minor cleanup changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :cars
   has_many :parking_spaces, through: :cars
 
+  has_many :addresses
+
+  accepts_nested_attributes_for :addresses
+
   has_secure_password(validations: false)
 
   validates :name, presence: true
